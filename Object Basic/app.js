816938e075bd30717774948d1fa8c8607ef991d1
Yusuf <usuf7hahin@gmail.com>
2024-08-12 21:13:49 +0600

    object basic on the way

--- Object Basic/app.js
+++ Object Basic/app.js
@@ -5,7 +5,7 @@
 const list = "friend - list";
 
 const mySelf = {
-  //#  every key(property) of javascript hold value inisde object. that's value could be any data
+  //#  every key(property) of javascript must hold value inisde object. that's value could be any data...
   name: "Yusuf Shahin",
   age: 27,
   married: false,
@@ -35,11 +35,108 @@ const mySelf = {
 // console.log(mySelf["friend - list"]);
 
 //? hold the value of crush :-
-console.log(mySelf.returnCrush("javascript"));
+// console.log(mySelf.returnCrush("javascript"));
+
+//! ============
+//? create a varible
+//! ============
+
+const name = "yusuf";
+const age = [
+  {
+    myAge: 27,
+    nazAge: 25,
+  },
+];
+const currentAddress = {
+  thana: "kotoali",
+  district: "ctg",
+  river: {
+    fRev: "podma",
+    sRev: "megna",
+  },
+};
+const homeDistrict = "noakhali";
+
+// ============
+// addEverything in obj
+// ============
+
+const myBio = {
+  //// every varible hold value inside it
+  name,
+  age,
+  currentAddress,
+  homeDistrict,
+};
+
+// console.log(myBio);
+
+//! ============
+//? destructering obj
+//! ============
+
+// const pickAge = myBio.age[0];
+// const {myAge,nazAge} = pickAge
+// const {
+//   currentAddress: { thana,district },
+// } = myBio;
+// const {
+//   currentAddress : {river : { fRev :p , sRev:m } }
+// } = myBio
+
+//* console those thing
+
+// console.log(myBio);
+// console.log(thana, district);
+// console.log(myAge, nazAge);
+// console.log(p, m);
+
+//!==============
+//? this func
+//!==============
+
+//! What is this ?
+//? In gloval this === window
+
+function showThis() {
+  console.log(this);
+}
+// showThis()
+
+//* inside showThis func we console this . when we invoke showThis() it"s refer the global
+// showThis();
+
+function outsideShow() {
+  function insideShow() {
+    console.log(this);
+  }
+  insideShow();
+}
+
+//* same this is happen in here
+// outsideShow();
 
 //!==============
 //? this in object
 //!==============
+
+//? What happen when we create showThis() func inside a obj.
+
+const john = {
+  name: "jhon",
+  age: 35,
+  address: "san fransisco",
+  friend: ["bob", "anna", "susi"],
+  // showThis: showThis,
+
+  //* same thing
+  showThis: function () {
+    console.log(this);
+  },
+};
+
+// john.showThis();
 
 const yusuf = {
   fname: "Yusuf",
@@ -71,41 +168,6 @@ const sabqat = {
 // nazmul.fullName("Nazmul Hasan");
 // sabqat.fullName();
 
-//! What is this ?
-//? In gloval this === window
-
-function showThis() {
-  console.log(this);
-}
-
-//* inside showThis func we console this . when we invoke showThis() it"s refer the global
-// showThis();
-
-function outsideShow() {
-  function insideShow() {
-    console.log(this);
-  }
-  insideShow();
-}
-
-//* same this is happen in here
-// outsideShow();
-
-//? What happen when we create showThis() func inside a obj.
-
-const john = {
-  name: "jhon",
-  age: 35,
-  address: "san fransisco",
-  friend: ["bob", "anna", "susi"],
-  // showThis: showThis,
-
-  //* same thing
-  showThis: function () {
-    console.log(this);
-  },
-};
-
 //!==============
 //? return obj inside func :-
 //!==============
@@ -118,7 +180,7 @@ function returnObj(name, age, year) {
   };
 }
 
-console.log(returnObj("snigdha", 17, 2024));
+// console.log(returnObj("snigdha", 17, 2024));
 
 //* showthis function directly indicate the obj...
 // john.showThis();
@@ -144,32 +206,171 @@ let user = {
 //?  In JavaScript, a factory function is a function that returns an object. It's called a factory function because it creates and returns a new object each time it's called, just like a factory creates and produces new products.
 
 //* inside the parameter of func we set the key property, when we invoke the func we set the value of those key...
-function progLang(language, library, franwork) {
+function progLang(language, library, framwork) {
   return {
     // language: language,
     // library: library,
-    // franwork: franwork,
+    // framwork: framwork,
 
     //# same thing work...
     language,
     library,
-    franwork,
+    framwork,
     //# we dont use fullstack func outside of it
     fullStack: function () {
       console.log(
-        ` ${this.library} is the library of ${this.language} and    ${this.franwork} its framwork`
+        ` ${this.library} is the library of ${this.language} and ${this.framwork} its framwork`
       );
     },
   };
 }
 
-console.log(progLang("javascript", "react", "next"));
+// console.log(progLang("javascript", "react", "next"));
 
 const javascript = progLang("javascript", "react", "next");
-console.log(javascript);
+// console.log(javascript);
 
-javascript.fullStack();
+//* same thing write in two way :-
+// javascript.fullStack();
+// progLang("javascript", "react", "next").fullStack();
 
 //!==============
 //? constructor function
 //!==============
+//* A constructor function is a special type of function in programming that is used to create objects. It is typically used in object-oriented programming languages like JavaScript. The purpose of a constructor function is to set the initial values or properties of an object when it is created.
+
+//? Same thing using constructer func
+
+//// https://www.freecodecamp.org/news/snake-case-vs-camel-case-vs-pascal-case-vs-kebab-case-whats-the-difference/
+
+//* In conster function we dont return a object and always use pascal case when create the func
+function ProgLang(language, framwork) {
+  this.language = language;
+  this.framwork = framwork;
+  this.fullStack = function () {
+    console.log(`${this.framwork} is the framwork of ${this.language}`);
+  };
+  // console.log(this);
+}
+
+//? in constructer we must use new keyword... what is new key word
+//! https://www.freecodecamp.org/news/demystifying-javascripts-new-keyword-874df126184c/
+
+//# new - create a new object , point to it, omit return
+
+// console.log(ProgLang);
+
+const python = new ProgLang("Python", "Django");
+//# in the prototype we find the constructor
+console.log(python);
+
+// python.fullStack();
+
+// const php = new ProgLang("PHP", "Laravle");
+// console.log(php);
+// php.fullStack();
+
+//!==============
+//? constructor property
+//!==============
+// https://javascript.info/constructor-new
+
+//* All Objects in Javascript have access to constructor property that returns a constructor function that created it.
+//* built in constructor functions
+//* arrays and functions are objects in javascript
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = function () {
+    console.log(
+      `My full name is ${this.firstName} ${this.lastName} and I love C++`
+    );
+  };
+}
+
+// const jonny = new Person("jonny", "sanders");
+// console.log(jonny);
+
+//# everything add in constructor
+// console.log(jonny.constructor);
+// jonny.fullName();
+
+// const bob = {};
+// console.log(bob.constructor);
+// const lists = [];
+// console.log(lists.constructor);
+// const sayHi = function () {};
+// console.log(sayHi.constructor);
+
+//* same thing on different way :-
+// const susy = new jonny.constructor("susy", "carpenter");
+// susy.fullName();
+
+//!==============
+//? Prototypal Inheritance Model
+//!==============
+
+/*
+Javascript uses prototypal inheritance model. That means that every constructor function/class has a prototype property that is shared by every instance of the constructor/class. So any properties and methods or prototype can be acessed by every instance. prototype property returns a object
+*/
+
+function Account(name, initialBalance) {
+  this.name = name;
+  this.balance = initialBalance;
+}
+
+const dohn = new Account("dohn", 200);
+const boby = new Account("boby", 0);
+
+Account.prototype.bank = "CHASE";
+Account.prototype.deposit = function (amount) {
+  this.balance += amount;
+  console.log(`Hello ${this.name}, your balance is ${this.balance}`);
+};
+console.log(dohn);
+
+console.log(dohn.bank);
+console.log(boby);
+
+dohn.deposit(300);
+boby.deposit(1000);
+
+//!==============
+//? Property Lookup
+//!==============
+
+/*
+If child does not have ask parent
+Everything in JS is an Object
+*/
+
+function Account(name, initialBalance) {
+  this.name = name;
+  this.balance = initialBalance;
+  // this.bank = 'Bank Of America';
+}
+
+const steve = new Account("steve", 200);
+const bob = new Account("bob", 0);
+
+Account.prototype.bank = "CHASE";
+Account.prototype.deposit = function (amount) {
+  this.balance += amount;
+  console.log(`Hello ${this.name}, your balance is ${this.balance}`);
+};
+// console.log(steve.bank);
+console.log(bob);
+console.log({});
+console.log([]);
+
+//!==============
+//? class in js
+//!==============
+
+// class Account {}
+
+// // console.log(Account);
+
+// const myAcc = new Account();
+// console.log(myAcc);
